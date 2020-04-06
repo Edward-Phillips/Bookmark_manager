19319dc48a342f6cbe6ffb28594399b6fd250ed1
Edward Phillips <edd0ddi0@gmail.com>
2020-04-06 16:26:31 +0100

    test - Bookmark should return an array when given the all method - failing

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -5,6 +5,9 @@ require 'bookmark'
       it 'Bookmark class responds to the all method' do
         expect(Bookmark).to respond_to :all 
       end   
+      it " should give me an array when given the all method" do
+        expect(Bookmark.all).to be_instance_of(Array)
+      end
     end 
   # to see  if it is an array 
   # to see if they are bookmark instances 
